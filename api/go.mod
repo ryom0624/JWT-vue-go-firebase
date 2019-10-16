c32f52edc7f64bc833e4b3d5cc584aea74eeafc4
ryo-murakami <ryo-murakami@two2.jp>
2019-10-17 08:48:11 +0900

    create environment

---
+++ api/go.mod
@@ -0,0 +1,5 @@
+module vue-golang-firebase
+
+go 1.12
+
+require github.com/gin-gonic/gin v1.4.0 // indirect
---
+++ api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "pong"})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("ping", ping)
+	router.Run(":8081")
+}
